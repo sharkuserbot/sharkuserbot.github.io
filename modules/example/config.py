b1b685cb5e78c01ebd921323698db10a4598b2db
sharkuserbot <122558967+sharkuserbot@users.noreply.github.com>
2023-01-13 01:18:35 +0200

    Add files via upload

---
+++ modules/example/config.py
@@ -0,0 +1,7 @@
+settings = {
+    'name': 'example',
+    'description': 'An example module.',
+    'by': 'MasterStroke777',
+    'source': 'http://sharkuserbot.github.io/modules/example/example.py',
+    'commands': '.example | .example1'
+}
