19d6228f9865670307a840a3e4867eb3581c6ba6
sharkuserbot <122558967+sharkuserbot@users.noreply.github.com>
2023-01-15 15:49:24 +0200

    Delete modules/quotes directory

--- modules/quotes/config.py
+++
@@ -1,5 +0,0 @@
-name = 'Quote'
-description = 'Generate stickers from message.'
-by = '@MasterStroke777'
-source = 'http://sharkuserbot.github.io/modules/quote/quote.py'
-commands = '▫️ <code>.q</code> <b>[reply]</b> 
