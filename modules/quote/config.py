905ce3995a5af3edaef3a0285d0b53363caf3e10
sharkuserbot <122558967+sharkuserbot@users.noreply.github.com>
2023-01-15 15:54:36 +0200

    Create config.py

---
+++ modules/quote/config.py
@@ -0,0 +1,7 @@
+quote = {
+  'name': 'quote',
+  'description': 'Generate stickers from message.',
+  'by': '@MasterStroke777',
+  'source': 'http://sharkuserbot.github.io/modules/quote/quote.py',
+  'commands': '▫️ <code>.q</code> <b>[reply]</b>'
+}  
