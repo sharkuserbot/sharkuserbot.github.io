a97a9024f3a8cd7ef4922e652f844500d26eeeb1
sharkuserbot <122558967+sharkuserbot@users.noreply.github.com>
2023-01-15 05:06:52 +0200

    Update config.py

--- modules/quotes/config.py
+++ modules/quotes/config.py
@@ -1,5 +1,7 @@
-'name': 'Quote',
-'description': 'Generate stickers from message.',
-'by': '@MasterStroke777',
-'source': 'http://sharkuserbot.github.io/modules/quote/quote.py',
-'commands': '▫️ <code>.q</code> <b>[reply]</b> '
+{
+ 'name': 'Quote',
+ 'description': 'Generate stickers from message.',
+ 'by': '@MasterStroke777',
+ 'source': 'http://sharkuserbot.github.io/modules/quote/quote.py',
+ 'commands': '▫️ <code>.q</code> <b>[reply]</b> '
+}
