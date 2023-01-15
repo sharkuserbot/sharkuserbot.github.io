6266acc48d25c8943902cd8aab3576bc6a41d6aa
sharkuserbot <122558967+sharkuserbot@users.noreply.github.com>
2023-01-15 23:15:14 +0200

    Update and rename config.py to config.txt

--- modules/example/config.py
+++
@@ -1,7 +0,0 @@
-{
-    'name': 'example',
-    'description': 'An example module.',
-    'by': '@MasterStroke777',
-    'source': 'http://sharkuserbot.github.io/modules/example/example.py',
-    'commands': '▫️ <code>.example</code> \n▫️ <code>.example1</code> <b>[text]</b> '
-}
