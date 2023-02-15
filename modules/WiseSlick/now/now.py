389724156312d516f998c6bb026bdcdb9071e4fe
sharkuserbot <122558967+sharkuserbot@users.noreply.github.com>
2023-02-16 01:09:28 +0200

    Update now.py

--- modules/WiseSlick/now/now.py
+++ modules/WiseSlick/now/now.py
@@ -8,9 +8,9 @@ import asyncio
 from pyrogram import Client , filters
 import requests , time
 import datetime
-from ..settings.main_settings import module_list, file_list
+from sharkub.settings.main_settings import module_list, file_list
 
-from ..settings.prefix import my_prefix
+from sharkub.settings.prefix import my_prefix
 prefix = my_prefix()
 
 @Client.on_message(filters.command("now", prefixes=prefix) & filters.me)
